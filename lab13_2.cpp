089a2d836b25aa3f30d1d0940a17fc5d38de9c29
Wimwipa780 <wimwipha4746@gmail.com>
2025-01-01 15:55:16 +0700

    lab_13.2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -11,7 +11,7 @@ void showImage(const bool [][M]);
 
 int main()
 {
-    bool image[N][M] = {};
+    bool image[N][M] s= {};
     int s,x,y;
     do{
         showImage(image);
@@ -20,4 +20,32 @@ int main()
         updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void showImage(const bool pic[][M]){
+    cout<<"------------------------------------------------------------------------"<<"\n";
+    
+    for(int i=0;i<30;i++){
+        cout << "|";
+        for(int j=0;j<70;j++){
+            if(pic[i][j]==0){
+                cout<<" ";
+            }else{
+                cout<<"*";
+            }
+        }
+        cout << "|" <<"\n";
+    }
+
+    cout<<"------------------------------------------------------------------------"<<"\n";
+}
+
+void updateImage(bool pic[][M],int s,int x,int y){
+    for(int i =0;i<30;i++){
+        for(int j=0;j<70;j++){
+            if ((sqrt(pow(i-x,2)+pow(j-y,2))) <= (s-1)){
+                pic[i][j]=1;
+            }
+        }
+    }
 }
